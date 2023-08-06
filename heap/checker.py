78c982aced6f8130c2979d04261ba37bbf996478
chhongzh <>
2023-08-06 19:48:02 +0800

    i18n

--- heap/checker.py
+++ heap/checker.py
@@ -3,11 +3,11 @@ from . import Lexer
 from .token import Token
 
 MESSAGE = {
-    "redundant func": "多余的endfunc关键字",
-    "unclosed func": "没有关闭的func标签",
-    "redundant command": "多余的endcommand关键字",
-    "unclosed command": "没有关闭的command标签",
-    "not int": "不是个数字",
+    "未打开func": "多余的endfunc关键字",
+    "未关闭func": "没有关闭的func标签",
+    "未打开command": "多余的endcommand关键字",
+    "未关闭command": "没有关闭的command标签",
+    "不是数字": "不是个数字",
     "": "",
 }
 
@@ -17,7 +17,7 @@ def syntax_check(text: str):
     try:
         toks = lexer.lex()
     except ValueError:
-        return "not int", -1
+        return "不是数字", -1
 
     toks: list[Token]
     pos = -1
@@ -34,18 +34,18 @@ def syntax_check(text: str):
                 func_level -= 1
 
                 if func_level < 0:
-                    return "redundant func", pos
+                    return "未打开func", pos
             elif tok.value == "command":
                 command_level += 1
             elif tok.value == "endcommand":
                 command_level -= 1
 
                 if command_level < 0:
-                    return "redundant command", pos
+                    return "未打开command", pos
 
     if func_level > 0:
-        return "unclosed func", -1
+        return "未关闭func", -1
     if command_level > 0:
-        return "unclosed command", -1
+        return "未关闭command", -1
 
     return "", -1
