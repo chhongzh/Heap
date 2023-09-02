e921551fbdd831c15c96565af36359725e468f94
chhongzh <>
2023-09-02 19:55:06 +0800

    Random模块

--- heap/stdlibs.py
+++ heap/stdlibs.py
@@ -1,4 +1,10 @@
-LIBS = {"math": "_math", "array": "_arrayutils", "type": "_type", "system": "_system"}
+LIBS = {
+    "math": "_math",
+    "array": "_arrayutils",
+    "type": "_type",
+    "system": "_system",
+    "random": "_random",
+}
 HEAP_LIBS = {
     "functionic": "functionic.heap",
     "endl": "endl.heap",
