d67ab2e4c961b5f8d2db83e32f9dd785763fe666
chhongzh <>
2023-09-02 18:02:15 +0800

    修复repr问题

--- heap/heapack/__init__.py
+++ heap/heapack/__init__.py
@@ -1,3 +1,10 @@
 from .packer import Packer
 from .unpacker import Unpacker
-from .check import *
+from .check import check_package_name, check_version
+
+__all__ = [
+    "Packer",
+    "Unpacker",
+    "check_package_name",
+    "check_version",
+]
