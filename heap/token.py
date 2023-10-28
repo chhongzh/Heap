345359857eb239be39ae13225c00c37885d694f1
chhongzh <>
2023-10-28 12:37:25 +0800

    MetaInfo

--- heap/token.py
+++ heap/token.py
@@ -1,7 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class MetaInfo:
+    start_pos: int
+    end_pos: int
+
+
 class Token:
-    def __init__(self, type, value):
+    def __init__(self, type: int, value, meta: MetaInfo):
         self.type = type
         self.value = value
+        self.meta = meta
 
     def __repr__(self) -> str:
-        return f"Token(type={self.type},value={self.value})"
+        return f"Token(type={repr(self.type)},value={repr(self.value)},meta={repr(self.meta)})"
