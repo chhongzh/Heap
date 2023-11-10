6be83424dda12a7cbaa40a51399ccffcae0d5996
chhongzh <>
2023-11-10 13:32:59 +0800

    注释:规范注释

--- heap/hook.py
+++ heap/hook.py
@@ -1,20 +1,20 @@
 # Heap @ 2023
 # chhongzh
 
-"Heap的钩子"
+""""Heap的钩子"""
 
 import sys
 from .eprint import print_error
 
 
 def print_val(val):
-    "print钩子"
+    """print钩子"""
 
     print(val, end="")
 
 
 def raise_error(error):
-    "抛出错误并退出"
+    """抛出错误并退出"""
 
     print_error(error)
 
