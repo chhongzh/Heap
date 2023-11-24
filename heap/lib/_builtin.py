da6dc1c6949af8d61cb438899cef6f9fb305535b
chhongzh <>
2023-11-24 19:40:11 +0800

    支持下标访问

--- heap/lib/_builtin.py
+++ heap/lib/_builtin.py
@@ -4,3 +4,7 @@ from ..log import info
 
 def length(father: Func | Root, val):
     father.stack.append(len(val))
+
+
+def visit(father: Func | Root, val, idx: int | str):
+    father.stack.append(val[idx])
