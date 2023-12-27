3e3483f7b4ea47b51bb72fdc47e80e54dd6b88e7
chhongzh <>
2023-12-27 21:49:37 +0800

    feat: 增加version提示

--- cli.py
+++ cli.py
@@ -12,12 +12,25 @@ import click
 from heap import Lexer, Builder, Runner
 from heap.repr import heap_repr
 from heap.loader import loader
+from heap.version_info import HEAP_VERSION_STR
+
+try:
+    from heap.compie_info import IS_COMPILE, COMPILE_DATE
+except ImportError:
+    IS_COMPILE = False
+    COMPILE_DATE = None
 
 from heap.log import HEAP_IO
 from os.path import dirname
 
 
 @click.group()
+@click.version_option(
+    version=f"{HEAP_VERSION_STR} (Not compile.)"
+    if not IS_COMPILE
+    else f"{HEAP_VERSION_STR} (Compiled on {COMPILE_DATE}.)",
+    prog_name="Heap",
+)
 def __wrapper():
     pass
 
