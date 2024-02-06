9c6c45dea63193421e4ce3b4f8e0da7142def279
chhongzh <2550519316@qq.com>
2024-02-06 19:52:06 +0800

    feat: 支持更多操作

--- heap/log.py
+++ heap/log.py
@@ -5,24 +5,32 @@
 提供了一些基本的日志
 """
 
-from logging import getLogger, StreamHandler, Formatter, WARN
+from logging import getLogger, StreamHandler, Formatter, WARN, DEBUG
 from io import StringIO
 from .version_info import HEAP_VERSION_STR
 
-HEAP_IO = StringIO()
-HEAP_LOGGER = getLogger("Heap")
-HEAP_HANDLER = StreamHandler(HEAP_IO)
-HEAP_FORMATTER = Formatter("[%(asctime)s] [%(levelname)5s]: %(message)s")
+if not globals().get("HEAP_LOGGING_IS_INIT", None):
+    HEAP_LOGGING_IS_INIT = True
+    HEAP_IO = StringIO()
+    HEAP_LOGGER = getLogger("Heap")
+    HEAP_HANDLER = StreamHandler(HEAP_IO)
+    HEAP_FORMATTER = Formatter("[%(asctime)s] [%(levelname)5s]: %(message)s")
 
-HEAP_HANDLER.setFormatter(HEAP_FORMATTER)
-HEAP_LOGGER.setLevel(WARN)
+    HEAP_HANDLER.setFormatter(HEAP_FORMATTER)
+    HEAP_LOGGER.setLevel(WARN)
 
-HEAP_LOGGER.addHandler(HEAP_HANDLER)
+    HEAP_LOGGER.addHandler(HEAP_HANDLER)
 
-debug = lambda msg: HEAP_LOGGER.debug(msg)
-info = lambda msg: HEAP_LOGGER.info(msg)
-warning = lambda msg: HEAP_LOGGER.warning(msg)
-error = lambda msg: HEAP_LOGGER.error(msg)
-critical = lambda msg: HEAP_LOGGER.critical(msg)
+    debug = lambda msg: HEAP_LOGGER.debug(msg)
+    info = lambda msg: HEAP_LOGGER.info(msg)
+    warning = lambda msg: HEAP_LOGGER.warning(msg)
+    error = lambda msg: HEAP_LOGGER.error(msg)
+    critical = lambda msg: HEAP_LOGGER.critical(msg)
 
-info(f"Heap V{HEAP_VERSION_STR}. Powered by Python. Made by chhongzh.")
+    module_debug = lambda md_name, msg: debug(f"[Module]: [{md_name}]: {msg}")
+    module_info = lambda md_name, msg: info(f"[Module]: [{md_name}]: {msg}")
+    module_warning = lambda md_name, msg: warning(f"[Module]: [{md_name}]: {msg}")
+    module_error = lambda md_name, msg: error(f"[Module]: [{md_name}]: {msg}")
+    module_critical = lambda md_name, msg: critical(f"[Module]: [{md_name}]: {msg}")
+
+    info(f"Heap V{HEAP_VERSION_STR}. Powered by Python. Made by chhongzh.")
