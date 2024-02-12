8967a31caff601a7fd445ab3fcf3da31d7413962
chhongzh <2550519316@qq.com>
2024-02-12 21:09:50 +0800

    feat: New cli for Heap V2

--- cli.py
+++ cli.py
@@ -2,27 +2,19 @@
 # coding=utf-8
 
 """
-Heap @ 2023
+Heap @ 2024
 
-这是一个基于Python开发的编程语言, 使用Python3.11开发
-
-你可以在https://github.com/chhongzh/Heap查看更多信息
+Heap V2, 
+New heights, better than Heap V1.
 """
 import click
 
-from heap import Lexer, Builder, Runner
-from heap.repl import heap_repl
-from heap import loader
-from heap.common import crack_deepth
-from heap.heapb import loader as byte_loader
-from heap.heapb import writer as byte_writer
-from heap.common import make_shebang
-from heap.version_info import HEAP_VERSION_STR
+from heap import lex
+from heap import reader
+from heap.analyzer import Analyzer
+from heap.runner import Runner
 from sys import platform as SYS_PLATFORM
-from pathlib import Path
 
-# !!! 防止递归深度
-crack_deepth()
 
 # 加强输入
 if SYS_PLATFORM != "win32":
@@ -34,15 +26,14 @@ except ModuleNotFoundError:
     IS_COMPILE = False
     COMPILE_DATE = None
 
-from heap.log import HEAP_IO
-from os.path import dirname
-
 
 @click.group()
 @click.version_option(
-    version=f"{HEAP_VERSION_STR} (Not compile.)"
-    if not IS_COMPILE
-    else f"{HEAP_VERSION_STR} (Compiled on {COMPILE_DATE}.)",
+    version=(
+        f"Heap V2 (Not compile.)"
+        if not IS_COMPILE
+        else f"HEAP V2 (Compiled on {COMPILE_DATE}.)"
+    ),
     prog_name="Heap",
 )
 def __wrapper():
@@ -51,66 +42,15 @@ def __wrapper():
 
 @__wrapper.command()
 @click.argument("filepath", type=click.Path(exists=True))
-@click.option("--showlog", default=False, is_flag=True)
-@click.argument("args", nargs=-1)
-def run(filepath, showlog, args):
-    """
-    运行Heap程序
-    """
+# @click.argument("args", nargs=-1)
+def run(filepath):
+    fdata = reader(filepath)
+    toks = lex(fdata).as_list()
 
-    is_byte = ".heapb" == Path(filepath).suffix
-
-    if not is_byte:  # Normal File mode
-        dt = loader(filepath)
-
-        l = Lexer(dt)
-        toks = l.lex()
-
-        b = Builder(toks, filepath)
-        root = b.parse()
-
-    else:
-        root = byte_loader(filepath)
-
-    r = Runner(root, dirname(filepath))
-    r.root.context["heap_argv"] = args
+    a = Analyzer(toks)
+    tree = a.parse()
+    r = Runner(tree)
     r.run()
-
-    if showlog:
-        print(HEAP_IO.getvalue(), end="")
-
-
-@__wrapper.command()
-@click.argument("filepath", type=click.Path(exists=True))
-@click.argument("outputpath", type=click.Path())
-def build(filepath, outputpath):
-    dt = loader(filepath)
-
-    l = Lexer(dt)
-    toks = l.lex()
-
-    b = Builder(toks, filepath)
-    root = b.parse()
-
-    byte_writer(root, f"{outputpath}.heapb")
-
-
-@__wrapper.command()
-def repl():  # pylint:disable=W0622
-    """REPL环境"""
-    heap_repl()
-
-
-@__wrapper.group()
-def config():
-    """提供一些配置生成"""
-
-
-@config.command()
-def shebang():
-    """生成shebang line"""
-
-    click.echo(f"#! {make_shebang()}")
 
 
 __wrapper()
